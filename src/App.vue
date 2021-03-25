e40e419180823651093a458deb5e25d5af382a11
Ponnenult <997764343@qq.com>
2021-03-25 23:35:38 +0800

    add:添加导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: false
+}
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: 'test1的专栏test1的专栏test1的专栏'
+    // avatar: ''
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: 'test2的专栏test1的专栏test1的专栏'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: 'test3的专栏test1的专栏test1的专栏'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: 'test4的专栏test1的专栏test1的专栏'
+  }
+]
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    return {
+      list: testData,
+      currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
